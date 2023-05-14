f37dd83286cd77a78992ec2be123390007cb9341
CP-ThinkDigital <coderplaysmarketing@gmail.com>
2023-05-14 14:26:51 +0530

    RTK Query

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import studyReducer from "../features/study/studySlice";
+import { studyFormApi } from "../services/studyFormApi";
 
 export const store = configureStore({
   reducer: {
     study: studyReducer,
+    [studyFormApi.reducerPath]: studyFormApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(studyFormApi.middleware),
 });
